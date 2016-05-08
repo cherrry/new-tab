3ce1a1234aa8678276ea3e585faf8876f5796054
Cherry Ng <cherry92@gmail.com>
2016-05-08 19:56:00 +0800

    Include moment to vendor script

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,7 +4,7 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin'
 export default {
   entry: {
     index: 'index',
-    vendor: ['cheerio', 'react', 'react-dom', 'react-css-modules', 'sprintf-js']
+    vendor: ['cheerio', 'moment', 'sprintf-js', 'react', 'react-dom', 'react-css-modules']
   },
   output: {
     filename: '[name].js',
