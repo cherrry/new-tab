c6ecc18267dcffd22daa74e28fa4b8098fc4d1a7
Cherry Ng <cherry92@gmail.com>
2016-05-10 11:22:39 +0800

    Simplify webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,61 +1,36 @@
 import webpack from 'webpack'
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
-export default [
-  {
-    entry: {
-      index: 'index',
-      vendor: ['react', 'react-dom', 'react-css-modules']
-    },
-    output: {
-      filename: '[name].js',
-      path: __dirname + '/dist/app',
-      publicPath: '/dist/app'
-    },
-    resolve: {
-      root: __dirname + '/src',
-      alias: {
-        bower: __dirname + '/bower_components',
-        icon: __dirname + '/src/icon/icon.scss'
-      }
-    },
-    module: {
-      loaders: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-        { test: /\.json$/, loader: 'json' },
-        { test: /\.s?css$/, loader: ExtractTextPlugin.extract('style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass') },
-        { test: /\.(eot|svg|ttf|woff2?)$/, loader: 'url' }
-      ]
-    },
-    plugins: [
-      new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
-      new ExtractTextPlugin('style.css', { allChunks: true })
+export default {
+  entry: {
+    index: 'index',
+    background: 'background',
+    react: ['react', 'react-dom', 'react-css-modules'],
+    libs: ['cheerio', 'lovefield']
+  },
+  output: {
+    filename: '[name].js',
+    path: __dirname + '/dist',
+    publicPath: '/dist'
+  },
+  resolve: {
+    root: __dirname + '/src',
+    alias: {
+      db: __dirname + '/src/data/db',
+      bower: __dirname + '/bower_components',
+      icon: __dirname + '/src/icon/icon.scss'
+    }
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
+      { test: /\.json$/, loader: 'json' },
+      { test: /\.s?css$/, loader: ExtractTextPlugin.extract('style?sourceMap', 'css?modules&importLoaders=1&localIdentName=[name]__[local]__[hash:base64:5]!sass') },
+      { test: /\.(eot|svg|ttf|woff2?)$/, loader: 'url' }
     ]
   },
-  {
-    entry: {
-      background: 'background',
-      vendor: ['lovefield', 'cheerio']
-    },
-    output: {
-      filename: '[name].js',
-      path: __dirname + '/dist/serv',
-      publicPath: '/dist/serv'
-    },
-    resolve: {
-      root: __dirname + '/src',
-      alias: {
-        db: __dirname + '/src/data/db'
-      }
-    },
-    module: {
-      loaders: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-        { test: /\.json$/, loader: 'json' }
-      ]
-    },
-    plugins: [
-      new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js')
-    ]
-  }
-]
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin(['react', 'libs'], '[name].js'),
+    new ExtractTextPlugin('style.css', { allChunks: true })
+  ]
+}
